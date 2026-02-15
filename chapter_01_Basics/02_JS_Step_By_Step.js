2823adcdc3cfc089b38228cbc905cff4e970f60f
Vikas Sharma <vikkytunerx8@gmail.com>
2026-02-15 13:43:37 +0530

    Initial commit with Playwright learning project

---
+++ chapter_01_Basics/02_JS_Step_By_Step.js
@@ -0,0 +1,13 @@
+let a = 10;
+console.log(a);
+
+for(let a=0; a<1000000; a++)
+{
+    console.log(a);
+}
+
+function print(){
+    console.log("Hello");
+}
+
+print();
---
+++ chapter_02_Java_Concepts/07_var_let_const.js
@@ -0,0 +1,12 @@
+// var, let, const
+
+// var - Function scope
+
+var a = 10; //Global Scope
+
+function printHello(){
+    console.log("Hello");
+
+}
+
+test();
